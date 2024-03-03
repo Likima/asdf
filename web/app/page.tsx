a0abb9c1dfe00a5b57023ec9f4fba05f8520bcc3
Brandon [D] <tangbrandonk@gmail.com>
2024-03-03 01:18:22 -0800

    Add social media icons and links to the page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,29 +1,51 @@
-const SOCIALS = [
+const INSTA = [
   {
-    text: "instagram",
+    text: "Instagram",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png",
+    handle: "brandon_tang_",
+    url: "https://www.instagram.com/brandon_tang_/",
   },
+];
+const DISCORD = [
   {
-    text: "discord",
+    text: "Discord",
     image:
       "https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png",
+    handle: "sowinghalo",
+    url: "https://discord.com",
+  },
+];
+
+const GITHUB = [
+  {
+    text: "GitHub",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/800px-GitHub_Invertocat_Logo.svg.png",
+    handle: "Likima",
+    url: "https://github.com/Likima",
   },
 ];
 
 function Icon({
   text,
   image,
+  handle,
+  url,
   size = 25,
 }: {
   text: string;
   image: string;
   size?: number;
+  handle: string;
+  url: string;
 }) {
   return (
     <div className="flex flex-row items-center gap-x-4 align-middle">
       <img src={image} width={size} height={size} alt={text} />
-      <span>{text}</span>
+      <a href={url}>
+        {text}: {handle}
+      </a>
     </div>
   );
 }
@@ -31,11 +53,40 @@ function Icon({
 export default function Page() {
   return (
     <div className="h-screen w-screen">
-      <div className="flex h-full w-full flex-col">
-        {SOCIALS.map(({ image, text }) => {
-          return <Icon image={image} text={text} />;
-        })}
-      </div>
+      <body className="bg-white font-sans dark:bg-slate-900">
+        <div className="mb-10 grid justify-items-center bg-sky-800 p-10 font-bold shadow-lg">
+          <h1 className="mb-5 text-3xl">Brandon Tang</h1>
+          <div className="grid grid-cols-3 justify-items-center">
+            <div className="basis-1/3 text-sky-600">
+              {GITHUB.map(({ image, text, handle, url }) => {
+                return (
+                  <Icon image={image} text={text} handle={handle} url={url} />
+                );
+              })}
+            </div>
+            <div className="basis-1/3 text-sky-600">
+              {INSTA.map(({ image, text, handle, url }) => {
+                return (
+                  <Icon image={image} text={text} handle={handle} url={url} />
+                );
+              })}
+            </div>
+            <div className="basis-1/3 text-sky-600">
+              {DISCORD.map(({ image, text, handle, url }) => {
+                return (
+                  <Icon image={image} text={text} handle={handle} url={url} />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <p className="m-10 text-2xl text-slate-900 dark:text-white">Projects</p>
+        <div className="flex flex-row ">
+          <div className="m-10 grid basis-1/4 justify-items-center bg-slate-900 p-10 dark:bg-white"></div>
+          <div className="basis-1/4 bg-slate-900 dark:bg-white">Hello</div>
+          <div className="basis-1/2 bg-slate-900 dark:bg-white">Hello</div>
+        </div>
+      </body>
     </div>
   );
 }
