38c3515b5fbbe34c1f3b7f7df9444d0b5144c24d
Brandon [D] <tangbrandonk@gmail.com>
2024-03-03 00:46:01 -0800

    first commit

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,3 +1,41 @@
+const SOCIALS = [
+  {
+    text: "instagram",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png",
+  },
+  {
+    text: "discord",
+    image:
+      "https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png",
+  },
+];
+
+function Icon({
+  text,
+  image,
+  size = 25,
+}: {
+  text: string;
+  image: string;
+  size?: number;
+}) {
+  return (
+    <div className="flex flex-row items-center gap-x-4 align-middle">
+      <img src={image} width={size} height={size} alt={text} />
+      <span>{text}</span>
+    </div>
+  );
+}
+
 export default function Page() {
-  return <div className="w-full bg-red-500">Hello!</div>;
+  return (
+    <div className="h-screen w-screen">
+      <div className="flex h-full w-full flex-col">
+        {SOCIALS.map(({ image, text }) => {
+          return <Icon image={image} text={text} />;
+        })}
+      </div>
+    </div>
+  );
 }
