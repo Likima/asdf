ad69076e434fd2a4a29f9266f18fc9c34b83345f
Brandon Zenbook <tangbrandonk@gmail.com>
2024-03-03 20:33:50 -0800

    Update "See More" button styling

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -78,7 +78,7 @@ function RenderCategories(){
   return CATEGORIES.map(({text}, index)=>{
     return(<div className="text-center mx-2 grid basis-1/3 justify-items-center bg-slate-900 p-5 dark:bg-slate-700 rounded-lg" key = {index}>
       <h1 className = "font-bold">{text}</h1>
-      <button className="bg-sky-400 m-2 px-4 py-2 shadow-xl rounded-lg">See More</button>
+      <button className="bg-sky-400 m-2 px-4 py-2 shadow-xl rounded-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">See More</button>
     </div>)
   })
 }
