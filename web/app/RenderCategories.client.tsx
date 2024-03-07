697a85430562984857172f3922c33307df25a3ab
Brandon Zenbook <tangbrandonk@gmail.com>
2024-03-07 11:12:39 -0800

    Refactor RenderCategories.client.tsx to improve button styling and functionality

--- web/app/RenderCategories.client.tsx
+++ web/app/RenderCategories.client.tsx
@@ -21,7 +21,9 @@ const CATEGORIES = [
       return (
         <div className="text-center mx-2 grid basis-1/3 justify-items-center bg-slate-900 p-5 dark:bg-slate-700 rounded-lg" key={index}>
           <h1 className="font-bold">{text}</h1>
-          <button onClick={() => setShowForm(true)}>See More</button>
+          <button className = "bg-sky-400 m-2 px-4 py-2 shadow-xl rounded-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300" 
+            onClick={
+                () => setShowForm(showForm ? false : true)}>See More</button>
           {showForm && <DisplayForm />}
         </div>
       );
